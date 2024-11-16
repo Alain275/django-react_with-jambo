01a383b91c0ac22cc81c3b23d28a566ab3d96417
Alain275 <alainshema275@gmail.com>
2024-11-16 10:08:46 +0200

    new

--- collaboration/main/models.py
+++ collaboration/main/models.py
@@ -6,14 +6,14 @@ class User(models.Model):
         YEAR_3="Y_3"
         YEAR_4="Y_4"
         YEAR_OF_STUDY_CHOICES={
-            Y_1:"year_1",
-            Y_2:"year_2",  
-            Y_3:"year_3",
-            Y_4:"year_4",
+             YEAR_1:"year_1",
+             YEAR_2:"year_2",  
+             YEAR_3:"year_3",
+             YEAR_4:"year_4",
         }
         
     
         fist_name=models.CharField(max_length=20) 
         last_name=models.CharField(max_length=20)
-        year_of_study=models.CharField(max_length=3,choices=YEAR_OF_STUDY_CHOICES,default=y_1)
+        year_of_study=models.CharField(max_length=3,choices=YEAR_OF_STUDY_CHOICES,default=YEAR_1)
 
